610033e556977bc044f8a863e1c3ca3a559d9e28
Cody McCarty <codyjmccarty@gmail.com>
2023-06-10 17:26:24 -0500

    bt task to clear bb values

---
+++ CSBTTask_ClearBlackboardValue.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CSBTTask_ClearBlackboardValue.h"
+
+UCSBTTask_ClearBlackboardValue::UCSBTTask_ClearBlackboardValue()
+{
+	NodeName = TEXT("CS Clear BlackBoard Value");
+}
