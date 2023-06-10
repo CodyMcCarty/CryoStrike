4ca57f392d31e2f5d3133a69b202c213e5e6ded5
Cody McCarty <codyjmccarty@gmail.com>
2023-06-09 20:32:56 -0500

    started gun

---
+++ CSGun.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CSGun.h"
+
+// Sets default values
+ACSGun::ACSGun()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Root);
+
+}
+
+// Called when the game starts or when spawned
+void ACSGun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACSGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
